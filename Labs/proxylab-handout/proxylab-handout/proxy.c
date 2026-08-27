31973d51beba5d76577b9b468a33123d5f522e59
TommyuY <tommyyu0326@gmail.com>
2026-08-26 22:33:33 -0700

    basic done

--- Labs/proxylab-handout/proxylab-handout/proxy.c
+++ Labs/proxylab-handout/proxylab-handout/proxy.c
@@ -8,6 +8,7 @@
 
 void *thread(void *vargp); 
 int parse_uri(char *uri, char *host, char *port, char *path);
+void service(int fd);
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
@@ -20,34 +21,14 @@ static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64;
 
 int main(int argc, char *argv[])
 {   
-
-    // char *testcase[10] = {
-    //     "http://www.cmu.edu:8080/hub/index.html",
-    //     "http://www.cmu.edu:8080/index.html",
-    //     "http://www.cmu.edu:8080",
-    //     "http://www.cmu.edu/",
-    //     "http://www.cmu.edu/a:b/c",
-    //     "http://www.cmu.edu/x?t=1:2",
-    //     "http://www.cmu.edu/s?q=cs&n=2",
-    //     "http://www.cmu.edu:8080/p?x=1#frag",
-    //     "www.cmu.edu/index.html"
-    // };
-
-    
-
-    // int n = sizeof(testcase)/ sizeof(testcase[0]);
-    // for (int i = 0; i < n; i++) {
-    //     char host[MAXLINE], port[PORTLEN], path[MAXLINE];
-    //     parse_uri(testcase[i], host, port, path);
-    //     printf("%s:%s/%s\n", host, port, path);
-    // }
-    
-
     /* Check command-line args */
     if (argc != 2) {
         fprintf(stderr, "usage: %s <port>\n", argv[0]);
         exit(1);
     }
+
+    /* A client that disappears mid-response must not kill the proxy */
+    signal(SIGPIPE, SIG_IGN);
 
     printf("%s", user_agent_hdr);
 
@@ -77,10 +58,10 @@ int main(int argc, char *argv[])
         }
         close(listenfd);
     }
-        
-    if (!p) { freeaddrinfo(listp); return -1; }
-    freeaddrinfo(listp);
 
+    freeaddrinfo(listp);
+    if (!p) { return -1; } //P ran to end of linked list
+    
     if (listen(listenfd, LISTENQ) < 0) {
         close(listenfd);
         return -1;
@@ -98,9 +79,6 @@ int main(int argc, char *argv[])
         close(connfd);
     }
 
-
-
-
     return 0;
 }
 
@@ -108,12 +86,18 @@ void service(int fd) {
     rio_t rio; 
     char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
 
-    Rio_readinitb(&rio, fd);
-    Rio_readlineb(&rio, buf, MAXLINE);
+    rio_readinitb(&rio, fd); // Init rio and its buffer
+    if (rio_readlineb(&rio, buf, MAXLINE) <= 0) { // client hung up or errored
+        return;
+    }
 
     printf("Request headers:\n");
     printf("%s", buf);
-    sscanf(buf, "%s %s %s", method, uri, version);
+    // Parse first line GET URL Version
+    if (sscanf(buf, "%s %s %s", method, uri, version) != 3) {
+        printf("Malformed request line\n");
+        return;
+    }
 
     if (strcasecmp(method, "GET")) { // Return 1 when !match
         printf("Proxy does not implement this method");
@@ -124,29 +108,33 @@ void service(int fd) {
     char hostname[MAXLINE], port[PORTLEN], path[MAXLINE];
     if (parse_uri(uri, hostname, port, path) < 0) {
         printf("Invalid URI\n");
-        close(fd);
+        return;
     }
 
-    char host_hdr[MAXLINE] = "";
+    char host_hdr[MAXLINE] = ""; //'\0'
     char other_hdrs[MAXLINE] = "";
     char hdr[MAXLINE];
 
-    while (Rio_readlineb(&rio, hdr, MAXLINE) > 0) {
+    ssize_t hn;
+    while ((hn = rio_readlineb(&rio, hdr, MAXLINE)) > 0) {
         if (!strcmp(hdr, "\r\n")) {
             break;
         }
-        if (!strncasecmp(hdr, "Host:", 5)) {
+        if (!strncasecmp(hdr, "Host:", 5)) { // if host exist, parse to host_hdr 
             snprintf(host_hdr, MAXLINE, "%s", hdr);
         }
         else if (!strncasecmp(hdr, "User-Agent:", 11) || //drop these
-                !strncasecmp(hdr, "Connection:", 11) ||
-                !strncasecmp(hdr, "Proxy-Connection:", 17)) {
+                 !strncasecmp(hdr, "Connection:", 11) ||
+                 !strncasecmp(hdr, "Proxy-Connection:", 17)) {
             continue;
         }
         else {
-            strncat(other_hdrs, hdr, MAXLINE - strlen(other_hdrs) - 1);
+            strncat(other_hdrs, hdr, MAXLINE - strlen(other_hdrs) - 1); //append other headers
         }
-            
+    }
+
+    if (hn < 0) { // client vanished mid-request
+        return;
     }
 
     if (host_hdr[0] == '\0') {
@@ -154,12 +142,64 @@ void service(int fd) {
     }
 
 
+    /* Build the request */
+    char request[MAXLINE]; // "GET <path> HTTP/1.0\r\n Host: <hostname>\r\n ..."
+    snprintf(request, MAXLINE,
+        "%s %s HTTP/1.0\r\n%s%sConnection: close\r\nProxy-Connection: close\r\n%s\r\n",
+        method, path, host_hdr, user_agent_hdr, other_hdrs);
 
+    printf("=== forwarding ===\n%s=== end ===\n", request);
+    fflush(stdout);
+        
+    /* Open new connection */
+    int serverfd = open_clientfd(hostname, port);
+    if (serverfd < 0) {
+        return;
+    }
+    printf("connect to [%s]:[%s] -> %d\n", hostname, port, serverfd);
+    fflush(stdout);
+
+    // Write send request to new connection
+    if (rio_writen(serverfd, request, strlen(request)) < 0) {
+        close(serverfd);
+        return;
+    }
+
+    // New rio to read from connection
+    rio_t sio;
+    char sbuf[MAXLINE];
+    ssize_t n; 
+
+    rio_readinitb(&sio, serverfd);
+    while ((n = rio_readnb(&sio, sbuf, MAXLINE)) > 0) { // Read from server n
+        if (rio_writen(fd, sbuf, n) < 0) break;         // Write to client n 
+    }
+
+    close(serverfd);
 
 
 }
 
+    // char *testcase[10] = {
+    //     "http://www.cmu.edu:8080/hub/index.html",
+    //     "http://www.cmu.edu:8080/index.html",
+    //     "http://www.cmu.edu:8080",
+    //     "http://www.cmu.edu/",
+    //     "http://www.cmu.edu/a:b/c",
+    //     "http://www.cmu.edu/x?t=1:2",
+    //     "http://www.cmu.edu/s?q=cs&n=2",
+    //     "http://www.cmu.edu:8080/p?x=1#frag",
+    //     "www.cmu.edu/index.html"
+    // };
 
+    
+
+    // int n = sizeof(testcase)/ sizeof(testcase[0]);
+    // for (int i = 0; i < n; i++) {
+    //     char host[MAXLINE], port[PORTLEN], path[MAXLINE];
+    //     parse_uri(testcase[i], host, port, path);
+    //     printf("%s:%s/%s\n", host, port, path);
+    // }
 int parse_uri(char *uri, char *host, char *port, char *path) {
     char *p = strstr(uri, "://");
     p = p ? p + 3 : uri; // P points to start of www.
